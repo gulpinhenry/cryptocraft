82b7348bd5b0c4bb5ab69f75e6364ed72be19577
Alex Gibson <argibson02@gmail.com>
2021-12-04 20:10:59 -0800

    Relocated the t-mius time helper functions into the utils folder. Got the time helper functions working to populate the x-axis labels.

--- client/src/components/PortfolioGraphContainer.js
+++ client/src/components/PortfolioGraphContainer.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 // import './GraphContainer.css';
 import IndividualGraph from './PortfolioGraph.js';
-import { graphData } from '../utils/graphData.js';
+import { graphData } from '../utils/graphData';
+// const graphData = require('../../../server/utils/graphData');
 
 
 const GraphContainer = () => {
-    useEffect(() => {
-        console.log(graphData);
-        console.log("^ GC");
-        // need current portfolio selector code here
-    });
+    // useEffect(() => {
+    //     console.log(graphData);
+    //     console.log("^ GC");
+    //     // need current portfolio selector code here
+    // });
 
     return (
         <div className="container">
---
+++ client/src/utils/timeHelpers.js
@@ -0,0 +1,23 @@
+import moment from 'moment';
+
+export function hourTimeInterval(graphDataPoints) {
+    var intervalLabels = [];
+    let timeStart = moment();
+    for (var i = 0; i < graphDataPoints.length; i++) {
+        let unixT = (timeStart - (i * 3600000)); // time is milliseconds, thus, 3 digits are added on the end of each unix integer.
+        let timePoint = moment(unixT).format("M-D-YYYY, HH:00");
+        intervalLabels.push(timePoint);
+    }
+    return intervalLabels.reverse();
+}
+
+export function sixHourTimeInterval(graphDataPoints) {
+    var intervalLabels = [];
+    let timeStart = moment();
+    for (var i = 0; i < graphDataPoints.length; i++) {
+        let unixT = (timeStart - (i * 21600000)); // time is milliseconds, thus, 3 digits are added on the end of each unix integer.
+        let timePoint = moment(unixT).format("MMM-DD-YYYY, HH:00:00");
+        intervalLabels.push(timePoint);
+    }
+    return intervalLabels.reverse();
+}
