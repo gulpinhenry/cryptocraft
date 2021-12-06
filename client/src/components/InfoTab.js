5ee225e6da9ade3a90a1b8db6853a194416df73f
Daniel Vo <daniel.vo@berkeley.edu>
2021-12-05 19:46:02 -0800

    displaying data onto webpage

--- client/src/components/InfoTab.js
+++ client/src/components/InfoTab.js
@@ -2,17 +2,37 @@ import * as React from 'react';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
+import { useQuery } from '@apollo/client';
+import {GET_CRYPTODETAILS} from '../utils/queries';
 
 function preventDefault(event) {
     event.preventDefault();
 }
 
-export default function InfoTab() {
+export default function InfoTab(props) {
     // get our user data here or maybe portfolio
+    const { loading, data } = useQuery(GET_CRYPTODETAILS, {
+        variables: { pair: 'btc'}
+    });
+
+    let info = 'Loading';
+
+    if (loading) {
+        console.log(':D')
+    } else {
+        info = data.cryptoDetails.cryptoInfo;
+       console.log(info);
+    }
+
     return (
         <React.Fragment>
             <Title>My Portfolios</Title>
             <Typography component="p" variant="h4">
+                {info.dailyChange}
+                {info.weeklyChange}
+                {info.yearlyChange}
+                {info.yearly_high}
+                {info.yearly_low}
                 Add more crypto data here
                 for portfolio this would be a pie chart
             </Typography>
