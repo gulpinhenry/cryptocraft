0f45d4174cb42ce258bfad83eeb1a37720be487f
Henry Kam <henry.j.kam@gmail.com>
2021-12-07 03:07:35 -0800

    configure server

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+
 app.get('*', (req, res) => {
 res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
